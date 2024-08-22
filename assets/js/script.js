039020f54defd8a9586215519fab9ce0537faa52
Viki Mulhall <vikimulhall@gmail.com>
2024-08-22 08:20:28 +0000

    add random numbers to game

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,24 +1,26 @@
+
 // Wait for the DOM to finish loading before running the game
-// Get the button elements and add event listereners to them
+// Get the button elements and add event listeners to them
 
 document.addEventListener("DOMContentLoaded", function() {
     let buttons = document.getElementsByTagName("button");
 
-    for (let button of buttons){
-        button.addEventListener("click", function(){
+    for (let button of buttons) {
+        button.addEventListener("click", function() {
             if (this.getAttribute("data-type") === "submit") {
                 alert("You clicked Submit!");
             } else {
                 let gameType = this.getAttribute("data-type");
-                alert('You clicked ${gameType}');
+                alert(`You clicked ${gameType}`);
             }
-        }
-    
+        });
     }
-})
+});
 
-function runGame(){
-
+function runGame() {
+    //Creates two random numbers between 1 and 25
+    let num1 = Math.floor(Math.random() * 25) +1;
+    let num2 = Math.floor(Math.random() * 25) +1;
 }
 
 function checkAnswer() {
@@ -46,9 +48,5 @@ function displaySubtractQuestion() {
 }
 
 function displayMultiplyQuestion() {
-
-}
-
-function displayDivisionQuestion() {
     
 }
